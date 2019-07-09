01999032a17386c57633f4dba6a23bfcb3f02cc0
Jeremy Farnault <haskkor@gmail.com>
2019-07-09 19:26:35 +1200

    Starting to work on branching

---
+++ branching.py
@@ -0,0 +1,8 @@
+is_dark = input('is it dark outside? (yes/no) ')
+if is_dark == 'yes':
+    print('Goodnight! Zzzzzzzzzzzzzzz.... ')
+tentacles = input('Do you have tentacles? (no/yes) ')
+if tentacles == 'yes':
+    print('I)
+    
+          
