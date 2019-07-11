0aafff107b48cf29f2a4105bd68e439946270168
Jeremy Farnault <haskkor@gmail.com>
2019-07-11 19:15:33 +1200

    Branching exercise ok

--- branching.py
+++ branching.py
@@ -1,8 +1,21 @@
 is_dark = input('is it dark outside? (yes/no) ')
 if is_dark == 'yes':
     print('Goodnight! Zzzzzzzzzzzzzzz.... ')
+
 tentacles = input('Do you have tentacles? (no/yes) ')
 if tentacles == 'yes':
-    print('I)
+    print('I never knew ostopuses could type!')
+else:
+    print('Greetings, human!')
+
+weather_today = input('rain / sun / snow')
+if weather_today == 'rain':
+    print('coat')
+elif weather_today == 'sun':
+    print('bikini')
+elif weather_today == 'snow':
+    print('ski')
+else:
+    print('Not correct')
     
           
