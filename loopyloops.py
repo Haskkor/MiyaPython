acdd3fb8827a68fa0c1649e7dae895b34efff43c
Jeremy Farnault <haskkor@gmail.com>
2019-07-13 15:41:06 +1200

    Worked on loops

---
+++ loopyloops.py
@@ -0,0 +1,13 @@
+for counter in range(2, 5) :
+    print('Miya\'s teddy - Don\'t touch!!!!!!')
+
+hippos = 9
+answer = 'yes'
+while answer == 'yes':
+    hippos = hippos + 1
+    print(str(hippos) + ' balancing hippos!!!')
+    answer = input('Add another hippo? (no/yes)')
+
+while True:
+    print('This is an infinite loop!')
+          
